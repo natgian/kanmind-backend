5d32d8e0b36c3c51a6390c9a7c61dc8b4e46f748
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-07-21 21:28:10 +0200

    chore: create auth_app and register in settings

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # External apps
+    'rest_framework',
+    # Local apps
+    'auth_app'
 ]
 
 MIDDLEWARE = [
