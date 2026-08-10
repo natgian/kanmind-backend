210062ab8c2aa3b244c4ed9f40a694944b706759
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-08-10 20:48:10 +0200

    feat: add CustomUser model with fullname field and run migrations

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+  """
+  Custom user model that extends Django's AbstractUser to include a fullname field required for the registration.
+  """
+  
+  fullname = models.CharField(max_length=255)
--- core/settings.py
+++ core/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom user model configuration
+AUTH_USER_MODEL = "auth_app.CustomUser"
