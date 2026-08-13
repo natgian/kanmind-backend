f24f397de4e16067db8221769b163103d1dbcb9f
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-08-13 15:04:31 +0200

    chore: install django-cors-headers

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 
 # Custom user model configuration
 AUTH_USER_MODEL = "auth_app.CustomUser"
+
+# Temporary fix for local development when opening HTML files directly
+CORS_ALLOWED_ORIGINS = [
+    "null",
+]
