ab4d101b9ba2899a342d61e6904391b6bc011aae
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-08-13 15:31:11 +0200

    feat(model): add Board model

--- boards_app/models.py
+++ boards_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Board(models.Model):
+  """Model representing a board."""
+  title = models.CharField(max_length=50)
+  owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="owned_boards")
+  members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="boards", blank=True)
+
+  def __str__(self):
+    return self.title
