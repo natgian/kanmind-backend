dc4ad79c882efc9333d0a22627e3e45b9bc8d6e6
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-08-28 12:04:12 +0200

    refactor(boards): refactor permissions for board members and owner

--- boards_app/api/permissions.py
+++ boards_app/api/permissions.py
@@ -2,9 +2,9 @@ from rest_framework import permissions
 
 class IsBoardOwnerOrMember(permissions.BasePermission):
   """
-  Allows access for GET and PATCH requests only if the user is the owner or a member of the board.
+  Allow access for 'retrieve' (GET detail) and 'partial_update' (PATCH) requests only if the user is the owner or a member of the board.
   
-  For DELETE request the user must be the owner of the board.
+  For 'destroy' (DELETE) request the user must be the owner of the board.
   """
   def has_object_permission(self, request, view, obj):
     is_owner = obj.owner == request.user
@@ -13,4 +13,7 @@ class IsBoardOwnerOrMember(permissions.BasePermission):
     if view.action in ["retrieve", "partial_update"]:
       return is_owner or is_member
 
-    return is_owner
+    if view.action in ["destroy"]:
+      return is_owner
+
+    return False
